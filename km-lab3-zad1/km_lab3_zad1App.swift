c94e42c599ec91d3a2ee8cde62c530962fd310e6
krzysztof.matyjaszczyk <krzysztof.matyjaszczyk@gmail.com>
2024-05-25 08:24:27 +0200

    Initial Commit

---
+++ km-lab3-zad1/km_lab3_zad1App.swift
@@ -0,0 +1,17 @@
+//
+//  km_lab3_zad1App.swift
+//  km-lab3-zad1
+//
+//  Created by student on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct km_lab3_zad1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
